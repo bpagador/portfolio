a011243c0d23fa7431fbbb30429c4b4ba54b331c
Briseida Pagador <bpagador@gmail.com>
2020-08-07 14:21:25 -0700

    text added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 export default function App() {
   return (
     <>
-      <h1>PORTFOLIO IN PROGRESS</h1>;
-      <h3>test</h3>
+      <h1>PORTFOLIO IN PROGRESS</h1>
+      <p>
+        <h3>PLEASE CHECK BACK SHORTLY</h3>
+      </p>
+      
     </>
 
   )
