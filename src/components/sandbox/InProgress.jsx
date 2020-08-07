92302f1d2da907faf53847afc733351173661c18
Briseida Pagador <bpagador@gmail.com>
2020-08-07 15:41:45 -0700

    attempting css change

--- src/components/sandbox/InProgress.jsx
+++ src/components/sandbox/InProgress.jsx
@@ -5,15 +5,16 @@ import '../../../assets/hummingbird-logo.png'
 const InProgress = () => {
 
     return (
-        <body>
-            <section className={styles.InProgress}>
+
+        <section className={styles.InProgress}>
             <h1>PORTFOLIO IN PROGRESS</h1>
+            
             <h3>I sincerely look forward to sharing my work with you soon!</h3>
             <h3>Thank you for your patience and do check back shortly.</h3>
+
             <h2>Cheers,</h2>
             <h2>Briseida Pagador</h2>
-            </section>
-        </body>
+        </section>    
         
     )
 };
