0243561beb5f57047351c906ff1a935156394cc0
Briseida Pagador <bpagador@gmail.com>
2020-08-07 14:19:32 -0700

    changing text for test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default function App() {
-  return <h1>Site Coming Soon</h1>;
+  return (
+    <>
+      <h1>PORTFOLIO IN PROGRESS</h1>;
+      <h3>test</h3>
+    </>
+
+  )
 }
   
