4e10616acfc3eb89b8acecde226137fb3c1e8168
Briseida Pagador <bpagador@gmail.com>
2020-08-07 14:39:25 -0700

    added message to splash page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
+import InProgress from '../sandbox/InProgress'
 
 export default function App() {
-  return (
-    <>
-      <h1>PORTFOLIO IN PROGRESS</h1>
-      <p>
-        <h3>PLEASE CHECK BACK SHORTLY</h3>
-      </p>
-      
-    </>
-
-  )
+  return <InProgress />
 }
   
---
+++ src/components/sandbox/InProgress.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './InProgress.css';
+
+const InProgress = () => {
+
+    return (
+        <section className={styles.InProgress}>
+            <h1>PORTFOLIO IN PROGRESS</h1>
+            <h3>I sincerely look forward to sharing my work with you soon!</h3>
+            <h3>Thank you for your patience and do check back shortly</h3>
+            <h4>Cheers,</h4>
+            <h4>Briseida Pagador</h4>
+        </section>
+    )
+};
+
+export default InProgress;
