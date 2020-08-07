f31d392467ed8fab0364405b54cb88616b3e2d7f
Briseida Pagador <bpagador@gmail.com>
2020-08-07 15:00:12 -0700

    added body tag

--- src/components/sandbox/InProgress.jsx
+++ src/components/sandbox/InProgress.jsx
@@ -4,13 +4,16 @@ import styles from './InProgress.css';
 const InProgress = () => {
 
     return (
-        <section className={styles.InProgress}>
+        <body>
+            <section className={styles.InProgress}>
             <h1>PORTFOLIO IN PROGRESS</h1>
             <h3>I sincerely look forward to sharing my work with you soon!</h3>
             <h3>Thank you for your patience and do check back shortly.</h3>
             <h2>Cheers,</h2>
             <h2>Briseida Pagador</h2>
-        </section>
+            </section>
+        </body>
+        
     )
 };
 
