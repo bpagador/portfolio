e1dd90430c4554415154a4410c2a6eb669c9b94e
Briseida Pagador <bpagador@gmail.com>
2020-08-22 15:17:35 -0700

    style project cards with flex

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -5,14 +5,15 @@ import PropTypes from 'prop-types';
 // eslint-disable-next-line react/prop-types
 const ProjectCard = (props) => (
   <section className={styles.ProjectCard}>
-    <h2>{props.title}</h2>
-    <p>
-      <a href={props.website} target="_blank" rel="noreferrer">{props.website}</a>
-    </p>
-    <a href={props.github} target="_blank" rel="noreferrer">
-      <img src="/assets/icons/github-2.png" alt="" width="30" height="30" />
-      <span>Source code</span>
-    </a>
+    <header>
+      <a href={props.website} target="_blank" rel="noreferrer">
+        <h2>{props.title}</h2>
+      </a>
+      <a href={props.github} target="_blank" rel="noreferrer">
+        <img src="/assets/icons/github-2.png" alt="" width="30" height="30" />
+        <span>View source</span>
+      </a>
+    </header>
     <a href={props.website} target="_blank" rel="noreferrer">
       <img src={props.logo} alt={props.title} width="200" height="200" />
     </a>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import Main from '../App/Main';
 import ProjectCard from './ProjectCard';
+import styles from './Projects.css';
 
 
 export default function Projects() {
-
-  const affirmE = '../../../assets/images/affirme-1.jpg';
-  const cryptoStimulus = '../../../assets/images/crypto.jpg';
-  const githubLogo = '../../../assets/icons/github-2.png';
   
   return (
-    <Main >
-      <div>
-        <h2>Projects</h2>
+    <Main>
+      <h2>Projects</h2>
 
+      <div className={styles.projectList}>
         <ProjectCard
           title="FetchArt"
           website="https://fetch-art.netlify.app/"
@@ -23,22 +20,24 @@ export default function Projects() {
           <p>FetchART recognizes our information overconsumption and aims to give the “power of scroll” a new meaning.</p>
         </ProjectCard>
 
-        {/* Convert the rest of these cards */}
-
-        <section>
-          <h3>Affirm.e</h3>
-          <img src={affirmE} alt="affirm.e logo" width="200" height="200"/>
+        <ProjectCard
+          title="Affirm.e"
+          website="https://twitter.com/affirm_e"
+          logo='/assets/images/affirme-1.jpg'
+          github="https://github.com/Affirm-e/affirmations-project"
+        >
           <p>The need for affirmations of our existence, of our health, and our worth are more important than ever--that is the ultimate goal of the Affirm.e Twitterbot.</p>
-          <img src={githubLogo} alt="github logo" width="30" height="30"/> 
-        </section>
-
-        <section>
-          <h3>CryptoStimulus</h3>
-          <img src={cryptoStimulus} alt="cryptoStimulus logo" width="500" height="200"/>
+        </ProjectCard>
+        
+        <ProjectCard
+          title="CryptoStimulus"
+          website="https://cryptostimulus.github.io/Project-Week-cryptoStimulus/"
+          logo='/assets/images/crypto.jpg'
+          github="https://github.com/cryptoStimulus/Project-Week-cryptoStimulus"
+        >
           <p>With wealth redistribution in mind, CryptoStimulus hosts a growing database of grassroots organizations from which the user can filter and select favorites. </p>
-          <img src={githubLogo} alt="github logo" width="30" height="30"/> 
-        </section>
-          
+        </ProjectCard>
+    
       </div>
     </Main>
   );
