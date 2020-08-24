cf3b1ca615162ce8f1e8361489f2584c0bf0ea4d
Briseida Pagador <bpagador@gmail.com>
2020-08-23 20:59:56 -0700

    trying curlies around resume

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -5,7 +5,7 @@ import styles from './Resume.css';
 export default function Resume() {
   return (
     <div className={styles.Resume}>
-      <a href='/assets/BriseidaPagadorResume.pdf' target="_blank" rel="noreferrer">
+      <a href={'/assets/BriseidaPagadorResume.pdf'} target="_blank" rel="noreferrer">
         Resume
       </a>
     </div>
