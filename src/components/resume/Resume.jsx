0339f255c286a8fb7c1fbd48cf40215517742e1d
Briseida Pagador <bpagador@gmail.com>
2020-08-23 20:02:03 -0700

    trying relative path same folder

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Resume.css';
 
+
 export default function Resume() {
   return (
     <div className={styles.Resume}>
-      <a href='../../../assets/BriseidaPagadorResume.pdf' target="_blank">
+      <a href='/BriseidaPagadorResume.pdf' target="_blank" rel="noreferrer">
         Resume
       </a>
     </div>
