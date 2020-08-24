851a0f9577380f3c4f78135d6d5a4058a4cac0d5
Briseida Pagador <bpagador@gmail.com>
2020-08-23 19:46:44 -0700

    removed no referrer to experiment

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -4,8 +4,8 @@ import styles from './Resume.css';
 export default function Resume() {
   return (
     <div className={styles.Resume}>
-      <a href='../../../assets/BriseidaPagadorResume.pdf' target="_blank" rel="noreferrer">
-                Resume
+      <a href='../../../assets/BriseidaPagadorResume.pdf' target="_blank">
+        Resume
       </a>
     </div>
   );
