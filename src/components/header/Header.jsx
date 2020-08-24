7e7e7afc90cb312b44d362c1020416d5e95b3e81
Briseida Pagador <bpagador@gmail.com>
2020-08-23 19:26:59 -0700

    added to home page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import styles from "./Header.css";
-import Resume from "../resume/Resume";
+import React from 'react';
+import styles from './Header.css';
+import Resume from '../resume/Resume';
 
 export default function Header() {
-    return (
-        <header className={styles.Header}>
-            <div>
-                <h2>fullstack software developer</h2>
-                <h1 className={styles.pahloMyPahlo}>
+  return (
+    <header className={styles.Header}>
+      <div>
+        <h2>fullstack software developer</h2>
+        <h1 className={styles.pahloMyPahlo}>
                     Briseida
-                    <br />
+          <br />
                     Pagador
-                </h1>
-            </div>
-            <Resume />
-            <img class={styles.logo} src="/assets/images/hummingbird-logo.png" alt="" />
-        </header>
-    );
+        </h1>
+      </div>
+      <Resume />
+      <img className={styles.logo} src="/assets/images/hummingbird-logo.png" alt="" />
+    </header>
+  );
 }
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import styles from "./Resume.css";
+import React from 'react';
+import styles from './Resume.css';
 
 export default function Resume() {
-    return (
-        <div class={styles.Resume}>
-            <a href="/assets/BriseidaPagadorResume.pdf" target="_blank" rel="noreferrer">
+  return (
+    <div className={styles.Resume}>
+      <a href="/assets/BriseidaPagadorResume.pdf" target="_blank" rel="noreferrer">
                 Resume
-            </a>
-        </div>
-    );
+      </a>
+    </div>
+  );
 }
