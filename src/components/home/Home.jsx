5608cf40df2a9a29786bb0030e7e3a12b73c46a7
Briseida Pagador <bpagador@gmail.com>
2022-04-03 14:53:30 -0700

    test commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,15 +4,10 @@ import Main from '../App/Main';
 export default function Home() {
   return (
     <Main>
-      <h2>[ brih · say · dah | she/her]</h2>
       <div>
-        <p>Hi, great to see you! Welcome to my Portfolio --v 1.1.</p>
+        <p>welcome and hello! here is my resume</p>
         <p>
-                    I hope we meet someday to talk about how important this year has been and how
-                    momentous that--despite radical unrest, uncertainty, and constant, forthcoming
-                    change--you landed here, to meet me. Thanks for stopping by, everything you need
-                    to know about me (for now) is here. Enjoy your stay and remember: this is just
-                    the beginning.
+          
         </p>
       </div><h2>[ skills ]</h2>
       <div>
